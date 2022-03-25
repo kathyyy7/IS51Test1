0cafe95fb25a062d1f5ef47b774445dea990b161
Kathy Franco <kfrancoba@mail.fresnostate.edu>
2022-03-25 16:42:08 -0700

    pseudo code commit

---
+++ test_1.py
@@ -0,0 +1,24 @@
+"""""
+The program is trying to determine which payment option is better (more money).
+First option is 100 dollars per pay for 10 days. The second pays 1 dollar a day with it being double each day for 10 days. 
+There will be two functions to calculate the pay rate
+Function1 will calculate for the first option and the function2 will calculate the second option
+
+the function1 will output 100 *= 10 days
+function2 will double the amount of that day, each day 
+
+If the amount is equal we output to the user that Option 1 and Option 2 is the same
+If option1 is better, we output "Option 1 is better"
+if option2 is bette, we output "Option 2 is better"
+
+
+"""""
+"""""
+option 1
+return 100 * 10
+
+option 2
+return day= n * 2
+
+
+"""""
